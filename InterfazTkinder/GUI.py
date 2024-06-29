e8d5341cfea0a7308cdf7b1f920beba2665fc9ae
Hamilton Durán <hamy.duran@gmail.com>
2024-06-28 21:30:52 -0600

    Cambiando el estilo de la ionterfaz y finalizaando combobox

--- InterfazTkinder/GUI.py
+++ InterfazTkinder/GUI.py
@@ -1,11 +1,11 @@
-#import tkinter as tk #Libreria para interfaz
-from tkinter import *
+#import tkinter as tk #Libreria para interfaz, si uso estilos default
+from tkinter import * #Agregando libreria tkinter
 import customtkinter #Para estilo de interfaz
 
 #CONFIGURACION DE VENTANA
 #window = tk.Tk() #Crear ventana con nombre Tkinter
 window = customtkinter.CTk() #Ahora en vez de poner button = tk.button sera customtkinter.CTKButton
-window.config() #Configuracion de estilos de la ventana
+window.config(borderwidth=10) #Configuracion de estilos de la ventana
 window.title("JPGtoDICOM Sender") #Titulo de la ventana
 window.geometry("400x700") #Dimensiones de la ventana
 window.resizable(0,0) #Configurando si o no se puede redimencionar la ventana 0 = false, 1 = True (width, height)
@@ -18,61 +18,56 @@ window.columnconfigure(1,weight=3)
 #CONFIGURACION DE LABELS DE DATOS DEL PACIENTE
 
 #Titulo
-fInput =customtkinter.CTkFrame(window, bg="gray", padx=5, pady=5, borderwidth=5, relief="sunken") #Creación de un cajon
-fInput.grid(row=0, column=0, rowspan=1, columnspan=2, sticky=tk.NSEW, padx=20, pady=20,) #Asignar y configurar celda del grid
-lInput = customtkinter.CTkLabel(master=fInput, text="INGRESAR DATOS DEL PACIENTE", font=("arial", 14, "bold italic"), bg="gray") #Label asignado al frame
+fInput =customtkinter.CTkFrame(window, corner_radius=10) #Creación de un cajon
+fInput.grid(row=0, column=0, rowspan=1, columnspan=2, sticky="nsew", padx=20, pady=20,) #Asignar y configurar celda del grid
+lInput = customtkinter.CTkLabel(master=fInput, text="INGRESAR DATOS DEL PACIENTE", font=("arial", 14)) #Label asignado al frame
 #lInput.config(font=("arial", 14, "bold italic")) <-------------- Otra forma de agregar cambios al frame, label, etc.
 lInput.pack() #Ejecutando el label
 
 #ID
-fId =customtkinter.CTkFrame(master=window)
-fId.grid(row=1, column=0, padx=10, pady=10)
-lId = customtkinter.CTkLabel(master=fId, text="ID del paciente:", font=("arial", 10))
+fId =customtkinter.CTkFrame(master=window, corner_radius=10)
+fId.grid(row=1, column=0, padx=5, pady=10, sticky="nsew")
+lId = customtkinter.CTkLabel(master=fId, text="ID del paciente:", font=("arial", 12))
 lId.pack()
 
 #Nombre
-fName =customtkinter.CTkFrame(master=window)
-fName.grid(row=2, column=0, padx=10, pady=10)
-lName = customtkinter.CTkLabel(master=fName, text="Nombre:", font=("arial", 10))
+fName =customtkinter.CTkFrame(master=window, corner_radius=10)
+fName.grid(row=2, column=0, padx=5, pady=10, sticky="nsew")
+lName = customtkinter.CTkLabel(master=fName, text="Nombre:", font=("arial", 12))
 lName.pack()
 
 #Apellido
-fLastname =customtkinter.CTkFrame(master=window)
-fLastname.grid(row=3, column=0, padx=10, pady=10)
-lLastname = customtkinter.CTkLabel(master=fLastname, text="Apellido:", font=("arial", 10))
+fLastname =customtkinter.CTkFrame(master=window, corner_radius=10)
+fLastname.grid(row=3, column=0, padx=5, pady=10, sticky="nsew")
+lLastname = customtkinter.CTkLabel(master=fLastname, text="Apellido:", font=("arial", 12))
 lLastname.pack()
 
 #Fecha
-fDate =customtkinter.CTkFrame(master=window)
-fDate.grid(row=4, column=0, padx=10, pady=10)
-lDate = customtkinter.CTkLabel(master=fDate, text="Fecha del Examen:", font=("arial", 10))
+fDate =customtkinter.CTkFrame(master=window, corner_radius=10)
+fDate.grid(row=4, column=0, padx=5, pady=10, sticky="nsew")
+lDate = customtkinter.CTkLabel(master=fDate, text="Fecha del Examen:", font=("arial", 12))
 lDate.pack()
-<<<<<<< HEAD
 
 #CONFIGURACION DE TEXTBOX
 
-fEntryId = tk.Frame(window)
-fEntryId.grid(row=1, column=1, padx=10, pady=10)
-entryId = tk.Entry(master=fEntryId)
-entryId.pack()
+fEntryId = customtkinter.CTkFrame(window)
+fEntryId.grid(row=1, column=1, padx=10, pady=10, sticky="nsew")
+entryId = customtkinter.CTkEntry(master=fEntryId)
+entryId.pack(fill=X)
 
-fEntryName = tk.Frame(window)
-fEntryName.grid(row=2, column=1, padx=10, pady=10)
-entryName = tk.Entry(master=fEntryName)
-entryName.pack()
+fEntryName = customtkinter.CTkFrame(window)
+fEntryName.grid(row=2, column=1, padx=10, pady=10, sticky="nsew")
+entryName = customtkinter.CTkEntry(master=fEntryName)
+entryName.pack(fill=X)
 
-fEntryLastname = tk.Frame(window)
-fEntryLastname.grid(row=3, column=1, padx=10, pady=10)
-entryLastname = tk.Entry(master=fEntryLastname)
-entryLastname.pack()
+fEntryLastname = customtkinter.CTkFrame(window)
+fEntryLastname.grid(row=3, column=1, padx=10, pady=10, sticky="nsew")
+entryLastname = customtkinter.CTkEntry(master=fEntryLastname)
+entryLastname.pack(fill=X)
 
-fEntryDate = tk.Frame(window)
-fEntryDate.grid(row=4, column=1, padx=10, pady=10)
-entryDate = tk.Entry(master=fEntryDate)
-entryDate.pack()
+fEntryDate = customtkinter.CTkFrame(window)
+fEntryDate.grid(row=4, column=1, padx=10, pady=10, sticky="nsew")
+entryDate = customtkinter.CTkEntry(master=fEntryDate)
+entryDate.pack(fill=X)
 
-
-=======
-
->>>>>>> 61d0721dfcf8fe52633979696c888e58824a4ce7
 window.mainloop() #Fin de app
