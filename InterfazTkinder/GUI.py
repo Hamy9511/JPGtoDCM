c0a8e5e04d5042b91434ab165b1e0c7c940f719a
Hamilton Durán <hamy.duran@gmail.com>
2024-06-28 19:08:13 -0600

    Cambios antes del pull

--- InterfazTkinder/GUI.py
+++ InterfazTkinder/GUI.py
@@ -1,7 +1,10 @@
-import tkinter as tk #Libreria para interfaz
+#import tkinter as tk #Libreria para interfaz
+from tkinter import *
+import customtkinter #Para estilo de interfaz
 
 #CONFIGURACION DE VENTANA
-window = tk.Tk() #Crear ventana con nombre Tkinter
+#window = tk.Tk() #Crear ventana con nombre Tkinter
+window = customtkinter.CTk() #Ahora en vez de poner button = tk.button sera customtkinter.CTKButton
 window.config() #Configuracion de estilos de la ventana
 window.title("JPGtoDICOM Sender") #Titulo de la ventana
 window.geometry("400x700") #Dimensiones de la ventana
@@ -15,34 +18,34 @@ window.columnconfigure(1,weight=3)
 #CONFIGURACION DE LABELS DE DATOS DEL PACIENTE
 
 #Titulo
-fInput = tk.Frame(window, bg="gray", padx=5, pady=5, borderwidth=5, relief="sunken") #Creación de un cajon
+fInput =customtkinter.CTkFrame(window, bg="gray", padx=5, pady=5, borderwidth=5, relief="sunken") #Creación de un cajon
 fInput.grid(row=0, column=0, rowspan=1, columnspan=2, sticky=tk.NSEW, padx=20, pady=20,) #Asignar y configurar celda del grid
-lInput = tk.Label(master=fInput, text="INGRESAR DATOS DEL PACIENTE", font=("arial", 14, "bold italic"), bg="gray") #Label asignado al frame
+lInput = customtkinter.CTkLabel(master=fInput, text="INGRESAR DATOS DEL PACIENTE", font=("arial", 14, "bold italic"), bg="gray") #Label asignado al frame
 #lInput.config(font=("arial", 14, "bold italic")) <-------------- Otra forma de agregar cambios al frame, label, etc.
 lInput.pack() #Ejecutando el label
 
 #ID
-fId = tk.Frame(master=window)
+fId =customtkinter.CTkFrame(master=window)
 fId.grid(row=1, column=0, padx=10, pady=10)
-lId = tk.Label(master=fId, text="ID del paciente:", font=("arial", 10))
+lId = customtkinter.CTkLabel(master=fId, text="ID del paciente:", font=("arial", 10))
 lId.pack()
 
 #Nombre
-fName = tk.Frame(master=window)
+fName =customtkinter.CTkFrame(master=window)
 fName.grid(row=2, column=0, padx=10, pady=10)
-lName = tk.Label(master=fName, text="Nombre:", font=("arial", 10))
+lName = customtkinter.CTkLabel(master=fName, text="Nombre:", font=("arial", 10))
 lName.pack()
 
 #Apellido
-fLastname = tk.Frame(master=window)
+fLastname =customtkinter.CTkFrame(master=window)
 fLastname.grid(row=3, column=0, padx=10, pady=10)
-lLastname = tk.Label(master=fLastname, text="Apellido:", font=("arial", 10))
+lLastname = customtkinter.CTkLabel(master=fLastname, text="Apellido:", font=("arial", 10))
 lLastname.pack()
 
 #Fecha
-fDate = tk.Frame(master=window)
+fDate =customtkinter.CTkFrame(master=window)
 fDate.grid(row=4, column=0, padx=10, pady=10)
-lDate = tk.Label(master=fDate, text="Fecha del Examen:", font=("arial", 10))
+lDate = customtkinter.CTkLabel(master=fDate, text="Fecha del Examen:", font=("arial", 10))
 lDate.pack()
 
 window.mainloop() #Fin de app
